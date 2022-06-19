1c01e31cb7664507a4ef6bbaaef0cb33d2b5ac40
RahulNathani49 <nathanirahul49@gmail.com>
2022-06-19 03:50:58 -0400

    changes new cart functionality added

---
+++ Readme.txt
@@ -0,0 +1,25 @@
+Shopping APP Project
+Team(Rahul,Omkar,Harmeet)
+Functionalities :
+1.) User Can logg in using the login credential username and password which is encrypted in the users database 
+(Please create username and password using test project of Shopping App Project).
+2.) After User Logs in the view of the products is shown which is fetched from the Inventory Database.
+3.) User Can Select the item from the list add the quantity and add it to the cart.After adding to the cart user can see the total price before the checkout from the cart
+4.) when user clicks on the checkout the items from the carts are placed as order and cart becomes empty . User can see the order history from the order history tab.
+
+Database :
+User1 : Its the table to store the data for users and also password in hash code;
+Inventory : products are stored in the inventory table
+Cart : All the items selected and added to cart and along with the quantity
+Orders : In orders table all the orders are saved along with the order id for a particular user 
+
+Run Query :
+Run the query given in the sqlquery(databasequery.sql) folder to create the database for the shopping app project  and sample values of the products.
+
+Individual Contributions :
+Harmeet - Entity Classes,Views UI(XAML),Services methods
+Omkar - Database design,Repository,Business Logic
+Rahul - ViewModels, Hashing Auth,Binding,Business Logic
+
+
+
--- sqlquery/databasequery.sql
+++ sqlquery/databasequery.sql
@@ -9,9 +9,9 @@ HashPassword varbinary(50) NOT NULL,
 Salt varbinary(50) NOT NULL,
 IsLoggedIn bit, 
 CreatedOn DateTime2 NOT NULL,
-) 
+);
 
-select * from Users1;
+
 
 create Table Inventory(
 ProductID int Identity(10001,1) Primary Key,
@@ -20,11 +20,12 @@ ProductCategory nvarchar(50) NOT NULL,
 ProductDescription nvarchar(50) NOT NULL,
 ProductPrice decimal not null,
 Discount decimal
-)
+);
 
 insert into dbo.inventory values ('Nike Shoes','Shoes','Formal Footwear',99.80,11.90);
 insert into dbo.inventory values ('Polo Shirt','Shirt','Formal white Shirt',55.80,10.00);
-
+insert into dbo.inventory values ('adidas jacket','jacket','Formal white jacket',25.80,3.00);
+insert into dbo.inventory values ('Polo jeans','jeans','black jeans',99.80,10.00);
 
 create table Cart(
 CartId int Identity,
@@ -32,7 +33,7 @@ UserId int NOT NULL,
 ProductID int NOT NULL,
 Quantity int NOT NULL,
 Primary Key(UserId,ProductId)
-)
+);
 
 ALTER TABLE Cart
 ADD FOREIGN KEY (ProductID) REFERENCES Inventory(ProductID);
@@ -40,12 +41,10 @@ ADD FOREIGN KEY (ProductID) REFERENCES Inventory(ProductID);
 create Table UserToOrderMap(
 UserId int Foreign Key references Users1(UserId) NOT NULL,
 OrderId int Primary kEY Identity(2000,1)
-)
+);
 
 create Table Orders(
 OrderId int Foreign Key references UserToOrderMap(OrderId),
-ProductId int Not NUll Foreign Key References Inventory(ProductID)
-)
-select * from Inventory;
-use ShoppingCart
-select orders.OrderId,Orders.quantity,ProductName,ProductPrice from Orders inner join Inventory on Inventory.ProductID = Orders.ProductId;
+ProductId int Not NUll Foreign Key References Inventory(ProductID),
+Quantity int not null
+);
